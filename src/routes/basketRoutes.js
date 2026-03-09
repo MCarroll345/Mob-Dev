d781fee568e5856a3a733c15b6aa0015fac65856
MARK CARROLL - STUDENT <g00419267@atu.ie>
2026-03-09 09:54:54 +0000

    Basket

--- src/routes/basketRoutes.js
+++ src/routes/basketRoutes.js
@@ -6,6 +6,6 @@ const basketController = require('../controllers/basketController');
 
 router.get('/get/:uid', basketController.getBasket);
 router.post('/add', basketController.addToBasket);
-router.delete('/del/:iid', basketController.removeItem);
+router.delete('/del', basketController.removeItem);
 
 module.exports = router;
